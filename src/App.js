388170f630d5b1992f0d1aafcc95dbe7d4b8ff29
anafletcher <153273057+anafletcher@users.noreply.github.com>
2024-06-02 22:46:21 +0100

    Added footer details and links

--- src/App.js
+++ src/App.js
@@ -14,7 +14,30 @@ export default function App() {
           <Dictionary defaultKeyword="Definition" />
         </main>
         <footer className="App-footer">
-          This project was coded by Alicia Fletcher
+          This project was coded by{" "}
+          <a
+            href="https://github.com/anafletcher"
+            default="_blank"
+            rel="noreferrer"
+          >
+            Alicia Fletcher
+          </a>
+          , is{" "}
+          <a
+            href="https://github.com/anafletcher/react-dictionary-project"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://react-project-dictionary-project.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            hosted on Netlify
+          </a>
         </footer>
       </div>
     </div>
